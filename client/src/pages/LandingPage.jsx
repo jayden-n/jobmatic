4754edc14568b180c1676e212c05453d764c887f
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-21 17:20:27 -0500

    refactor: add in styles for responsive landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -24,7 +24,7 @@ const LandingPage = () => {
 						Register
 					</Link>
 					<Link to='/login' className='btn'>
-						Login / Demo
+						Login / Demo Use
 					</Link>
 				</div>
 
@@ -34,6 +34,58 @@ const LandingPage = () => {
 	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.section`
+	nav {
+		width: var(--fluid-width);
+		max-width: var(--max-width);
+		margin: 0 auto;
+		height: var(--nav-height);
+		display: flex;
+		align-items: center;
+	}
+
+	.page {
+		min-height: calc(100vh - var(--nav-height));
+		display: grid;
+		align-items: center;
+		margin-top: -3rem;
+	}
+
+	h1 {
+		font-weight: 700;
+		span {
+			color: var(--primary-500);
+		}
+		margin-bottom: 1.5rem;
+	}
+
+	p {
+		line-height: 2;
+		color: var(--text-secondary-color);
+		margin-bottom: 1.5rem;
+		max-width: 35em;
+	}
+
+	.register-link {
+		margin-right: 1rem;
+	}
+
+	.main-img {
+		display: none;
+	}
+	.btn {
+		padding: 0.75rem 1rem;
+	}
+
+	@media (min-width: 992px) {
+		.page {
+			grid-template-columns: 1fr 400px;
+			column-gap: 3rem;
+		}
+		.main-img {
+			display: block;
+		}
+	}
+`;
 
 export default LandingPage;
