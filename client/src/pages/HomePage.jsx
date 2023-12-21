808449be61f131eda93dda32902e1d452cd2fb35
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-21 17:02:34 -0500

    feat: create landing page initial setup

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,8 +3,6 @@ import { Outlet } from "react-router-dom";
 const HomePage = () => {
 	return (
 		<>
-			<nav>navbar</nav>
-
 			{/* Outlet are children */}
 			<Outlet />
 		</>
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,4 +1,39 @@
+import styled from "styled-components";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
-	return <div>LandingPage</div>;
+	return (
+		<Wrapper>
+			<nav>
+				<img src={logo} alt='jobmatic' className='logo' />
+			</nav>
+			<div className='container page'>
+				<div className='info'>
+					<h1>
+						Job <span>Tracking</span> App
+					</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse
+						itaque autem hic cum voluptatem earum a quis! Commodi nam sunt quo
+						voluptatibus nemo nesciunt iure pariatur deleniti aut autem.
+						Quibusdam, totam quia.
+					</p>
+					<Link to='/register' className='btn register-link'>
+						Register
+					</Link>
+					<Link to='/login' className='btn'>
+						Login / Demo
+					</Link>
+				</div>
+
+				<img src={main} alt='landing image' className='img main-img' />
+			</div>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div``;
+
 export default LandingPage;
