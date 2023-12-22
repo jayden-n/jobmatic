08473d7fe9fd929cf565b0e8b2f05b360966920c
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-22 17:01:47 -0500

    refactor: implement responsive styling to register page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { FormRow, Logo } from "../components";
+import styled from "styled-components";
 
 const LoginPage = () => {
 	return (
-		<>
+		<Wrapper>
 			<form className='form'>
 				<Logo />
 				<h4>login</h4>
@@ -25,7 +26,44 @@ const LoginPage = () => {
 					</Link>
 				</p>
 			</form>
-		</>
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled.section`
+	min-height: 100vh;
+	display: grid;
+	align-items: center;
+
+	.logo {
+		display: block;
+		margin: 0 auto;
+		margin-bottom: 1.38rem;
+	}
+
+	.form {
+		max-width: 400px;
+		border-top: 5px solid var(--primary-500);
+	}
+
+	h4 {
+		text-align: center;
+		margin-bottom: 1.38rem;
+	}
+
+	p {
+		margin-top: 1rem;
+		text-align: center;
+		line-height: 1.5;
+	}
+	.btn {
+		margin-top: 1rem;
+	}
+	.member-btn {
+		color: var(--primary-500);
+		letter-spacing: var(--letter-spacing);
+		margin-left: 0.25rem;
+		text-decoration: underline;
+	}
+`;
 export default LoginPage;
