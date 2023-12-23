b2dbe9ddd8f46b05729321cb64a2eef63002e70f
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-23 08:21:25 -0500

    refactor: apply responive designs for navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,73 @@
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo";
+import { useDashboardContext } from "../hooks/useDashboardContext";
+import styled from "styled-components";
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const { toggleSidebar } = useDashboardContext();
+	return (
+		<Wrapper>
+			<div className='nav-center'>
+				<button type='button' className='toggle-btn' onClick={toggleSidebar}>
+					<FaAlignLeft />
+				</button>
+				<div>
+					<Logo />
+					<h4 className='logo-text'>dashboard</h4>
+				</div>
+
+				<div className='btn-container'>toggle/logout</div>
+			</div>
+		</Wrapper>
+	);
 };
+const Wrapper = styled.nav`
+	height: var(--nav-height);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.1);
+	background: var(--background-secondary-color);
+	.nav-center {
+		display: flex;
+		width: 90vw;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.toggle-btn {
+		background: transparent;
+		border-color: transparent;
+		font-size: 1.75rem;
+		color: var(--primary-500);
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+	}
+	.logo-text {
+		display: none;
+	}
+	.logo {
+		display: flex;
+		align-items: center;
+		width: 100px;
+	}
+	.btn-container {
+		display: flex;
+		align-items: center;
+	}
+
+	@media (min-width: 992px) {
+		position: sticky;
+		top: 0;
+		.nav-center {
+			width: 90%;
+		}
+		.logo {
+			display: none;
+		}
+		.logo-text {
+			display: block;
+		}
+	}
+`;
 export default Navbar;
