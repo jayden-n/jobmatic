21b9806ba0b2615c754cbd2d60596db5a9043cfe
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-27 17:40:02 -0500

    feat: extract NavLinks into separate component; create big sidebar structure

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,4 +1,27 @@
+import NavLinks from "./NavLinks";
+import Logo from "./Logo";
+import { useDashboardContext } from "../hooks/useDashboardContext";
+import styled from "styled-components";
+
 const BigSidebar = () => {
-	return <div>BigSideBar</div>;
+	const { showSidebar } = useDashboardContext();
+
+	return (
+		<Wrapper>
+			<div
+				className={
+					showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+				}
+			>
+				<div className='content'>
+					<header>
+						<Logo />
+					</header>
+					<NavLinks isBigSidebar />
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
+const Wrapper = styled.div``;
 export default BigSidebar;
--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,9 +1,8 @@
 import { FaTimes } from "react-icons/fa";
 import { useDashboardContext } from "../hooks/useDashboardContext";
 import Logo from "./Logo";
-import { links } from "../utils/links";
-import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
 	const { showSidebar, toggleSidebar } = useDashboardContext();
@@ -22,25 +21,7 @@ const SmallSidebar = () => {
 					<header>
 						<Logo />
 					</header>
-
-					<div className='nav-links'>
-						{links.map((link) => {
-							const { text, path, icon } = link;
-							return (
-								<NavLink
-									to={path}
-									key={text}
-									className='nav-link'
-									onClick={toggleSidebar}
-									// end: do not set current child to be 'active'
-									end
-								>
-									<span className='icon'>{icon}</span>
-									{text}
-								</NavLink>
-							);
-						})}
-					</div>
+					<NavLinks />
 				</div>
 			</div>
 		</Wrapper>
