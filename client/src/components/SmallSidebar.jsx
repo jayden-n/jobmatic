b922e3ab24eb41801176fdd695c04888f0fd2627
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-27 17:21:30 -0500

    refactor: style responsive sidebar with hover effects

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -3,15 +3,16 @@ import { useDashboardContext } from "../hooks/useDashboardContext";
 import Logo from "./Logo";
 import { links } from "../utils/links";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const SmallSidebar = () => {
 	const { showSidebar, toggleSidebar } = useDashboardContext();
 
 	return (
-		<div>
+		<Wrapper>
 			<div
 				className={
-					showSidebar ? "sidebar-container show-container" : "sidebar-container"
+					showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
 				}
 			>
 				<div className='content'>
@@ -42,7 +43,77 @@ const SmallSidebar = () => {
 					</div>
 				</div>
 			</div>
-		</div>
+		</Wrapper>
 	);
 };
+const Wrapper = styled.aside`
+	@media (min-width: 992px) {
+		display: none;
+	}
+
+	.sidebar-container {
+		position: fixed;
+		inset: 0;
+		background: rgba(0, 0, 0, 0.7);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: -1;
+		opacity: 0;
+		transition: var(--transition);
+		visibility: hidden;
+	}
+	.show-sidebar {
+		z-index: 99;
+		opacity: 1;
+		visibility: visible;
+	}
+	.content {
+		background: var(--background-secondary-color);
+		width: var(--fluid-width);
+		height: 95vh;
+		border-radius: var(--border-radius);
+		padding: 4rem 2rem;
+		position: relative;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+	.close-btn {
+		cursor: pointer;
+		position: absolute;
+		top: 10px;
+		left: 10px;
+		background: transparent;
+		border-color: transparent;
+		font-size: 2rem;
+		color: var(--red-dark);
+	}
+	.nav-links {
+		padding-top: 2rem;
+		display: flex;
+		flex-direction: column;
+	}
+	.nav-link {
+		display: flex;
+		align-items: center;
+		color: var(--text-secondary-color);
+		padding: 1rem 0;
+		text-transform: capitalize;
+		transition: var(--transition);
+	}
+	.nav-link:hover {
+		color: var(--primary-500);
+	}
+	.icon {
+		font-size: 1.5rem;
+		margin-right: 1rem;
+		display: grid;
+		place-items: center;
+	}
+	.active {
+		color: var(--primary-500);
+	}
+`;
+
 export default SmallSidebar;
