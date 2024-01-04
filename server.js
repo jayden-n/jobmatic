495bfc5dcd2d0a73cdafcc4d568862ed694d9f7c
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-04 16:40:51 -0500

    feat(api): finish CRUD operations by adding in update & delete job

--- server.js
+++ server.js
@@ -71,6 +71,41 @@ app.get("/api/v1/jobs/:id", (req, res) => {
 	res.status(200).json({ job });
 });
 
+//  ========= UPDATE JOB  =========
+app.patch("/api/v1/jobs/:id", (req, res) => {
+	const { company, position } = req.body;
+	if (!company || !position) {
+		return res.status(404).json({ msg: "please provide company and position" });
+	}
+
+	const { id } = req.params;
+	const job = jobs.find((job) => job.id === id);
+
+	if (!job) {
+		return res.status(404).json({ msg: `no job with id ${id}` });
+	}
+
+	job.company = company;
+	job.position = position;
+
+	res.status(200).json({ msg: "job modified!", job });
+});
+
+//  ========= DELETE JOB  =========
+
+app.delete("/api/v1/jobs/:id", (req, res) => {
+	const { id } = req.params;
+	const job = jobs.find((job) => job.id === id);
+	if (!job) {
+		return res.status(404).json({ msg: `no job with id ${id}` });
+	}
+
+	const newJobs = jobs.filter((job) => job.id !== id);
+	jobs = newJobs;
+
+	res.status(200).json({ msg: "job deleted" });
+});
+
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
