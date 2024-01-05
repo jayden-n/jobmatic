1e0c38644a1d87f53c176c2531bdac479c115ef3
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-05 16:34:07 -0500

    refactor: create more custom error handlers (bad req, unauthenticated, unauthorized)

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -7,3 +7,24 @@ export class NotFoundError extends Error {
 		this.statusCode = StatusCodes.NOT_FOUND;
 	}
 }
+export class BadRequestError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "BadRequestError";
+		this.statusCode = StatusCodes.BAD_REQUEST;
+	}
+}
+export class Unauthenticated extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "Unauthenticated";
+		this.statusCode = StatusCodes.UNAUTHORIZED;
+	}
+}
+export class UnauthorizedError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "UnauthorizedError";
+		this.statusCode = StatusCodes.FORBIDDEN;
+	}
+}
