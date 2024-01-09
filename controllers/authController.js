4e98caccc5e960c99df1d622129cfca6323ef90e
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-09 17:26:16 -0500

    feat: create logout controller with cookie token verification

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,3 +45,13 @@ export const login = async (req, res) => {
 
 	res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
+
+// ================== LOGOUT ==================
+
+export const logout = (req, res) => {
+	res.cookie("token", "logout", {
+		httpOnly: true,
+		expires: new Date(Date.now()),
+	});
+	res.status(StatusCodes.OK).json({ msg: "user logged out!" });
+};
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 import {
 	validateLoginInput,
 	validateRegisterInput,
@@ -9,5 +9,6 @@ const router = Router();
 
 router.route("/register").post(validateRegisterInput, register);
 router.route("/login").post(validateLoginInput, login);
+router.route("/logout").get(logout);
 
 export default router;
