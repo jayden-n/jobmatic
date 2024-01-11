12a249223d926065de9cfe7d32111b7ea2cb1f4f
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-11 16:42:53 -0500

    refactor: create add jobs page structure

--- client/src/pages/AddJobPage.jsx
+++ client/src/pages/AddJobPage.jsx
@@ -1,4 +1,34 @@
+import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import { FormRow } from "../components";
+
 const AddJobPage = () => {
-	return <div>AddJobPage</div>;
+	const { user } = useOutletContext();
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === "submitting";
+
+	return (
+		<div>
+			<Form method='post' className='form'>
+				<h4 className='form-title'>add job</h4>
+				<div className='form-center'>
+					<FormRow type='text' name='position' />
+					<FormRow type='text' name='company' />
+					<FormRow
+						type='text'
+						labelText='job location'
+						name='jobLocation'
+						defaultValue={user.location}
+					/>
+					<button
+						type='submit'
+						className='btn btn-block form-btn'
+						disabled={isSubmitting}
+					>
+						{isSubmitting ? "submitting..." : "submit"}
+					</button>
+				</div>
+			</Form>
+		</div>
+	);
 };
 export default AddJobPage;
