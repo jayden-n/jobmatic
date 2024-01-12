8c8eda60e1365139bff50e06822510d7703824ca
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-12 15:30:05 -0500

    feat: create jobs container content with responsive styling

---
+++ client/src/components/SingleJob.jsx
@@ -0,0 +1,4 @@
+const SingleJob = ({ job }) => {
+	return <div>SingleJob</div>;
+};
+export default SingleJob;
--- client/src/pages/AllJobsPage.jsx
+++ client/src/pages/AllJobsPage.jsx
@@ -1,15 +1,17 @@
 import { useLoaderData } from "react-router-dom";
 import { JobsContainer, SearchContainer } from "../components";
+import { createContext } from "react";
 
+export const AllJobsContext = createContext();
 const AllJobsPage = () => {
 	const data = useLoaderData();
-	console.log(data?.jobs);
 
 	return (
-		<>
+		<AllJobsContext.Provider value={{ data }}>
 			<SearchContainer />
 			<JobsContainer />
-		</>
+		</AllJobsContext.Provider>
 	);
 };
+
 export default AllJobsPage;
