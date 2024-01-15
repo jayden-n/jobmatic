5e22c8f2ba98c6a6569a08cd02555f8b836de41f
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-15 14:42:18 -0500

    refactor: add in job component styling

--- client/src/components/SingleJobInfo.jsx
+++ client/src/components/SingleJobInfo.jsx
@@ -9,5 +9,22 @@ const SingleJobInfo = ({ icon, text }) => {
 	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	align-items: center;
+	.job-icon {
+		font-size: 1rem;
+		margin-right: 1rem;
+		display: flex;
+		align-items: center;
+		svg {
+			color: var(--text-secondary-color);
+		}
+	}
+
+	.job-text {
+		text-transform: capitalize;
+		/* letter-spacing: var(--letter-spacing); */
+	}
+`;
 export default SingleJobInfo;
