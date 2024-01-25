519e1dbba60a9eab82f7e44c4891778a346dfb7e
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-25 15:58:43 -0500

    feat(server): implement pagination logic in getAllJobs controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -39,8 +39,25 @@ export const getAllJobs = async (req, res) => {
 	// based on dynamic values
 	const sortKey = sortOptions[sort] || sortOptions.newest;
 
-	const jobs = await Job.find(queryObject).sort(sortKey);
-	res.status(StatusCodes.OK).json({ jobs });
+	// setup pagination
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	// 1st page: (1 - 1) * 8 = 0
+	// 2nd page: (2 - 1) * 8 = 8 => skip the first 8 pages to display new 8 pages on the 2nd page
+	const skip = (page - 1) * limit;
+
+	const jobs = await Job.find(queryObject)
+		.sort(sortKey)
+		.skip(skip)
+		.limit(limit);
+	const totalJobs = await Job.countDocuments(queryObject);
+
+	// control how many pages you have on front-end:
+	// 99 / 10, last page just have 9 jobs only
+	const numOfPages = Math.ceil(totalJobs / limit);
+	res
+		.status(StatusCodes.OK)
+		.json({ totalJobs, numOfPages, currentPage: page, jobs });
 };
 
 // ================== GET SINGLE JOB  ==================
