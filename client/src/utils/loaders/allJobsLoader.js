50da9e550e52ce3a58c414036be021742cbb4cf5
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-25 18:29:11 -0500

    refactor: add in loader and query params (axios) to get jobs data from server

--- client/src/utils/loaders/allJobsLoader.js
+++ client/src/utils/loaders/allJobsLoader.js
@@ -1,10 +1,18 @@
 import customFetch from '../api/customFetch';
 import { toast } from 'react-toastify';
 
-export const allJobsLoader = async () => {
-	console.log('hello');
+export const allJobsLoader = async ({ request }) => {
+	// console.log(request.url);
+	const params = Object.fromEntries([
+		// turn the url query values into object
+		...new URL(request.url).searchParams.entries(),
+	]);
+
 	try {
-		const { data } = await customFetch.get('/jobs');
+		const { data } = await customFetch.get('/jobs', {
+			// pass in the params thru axios
+			params,
+		});
 		return data;
 	} catch (error) {
 		toast.error(error?.response?.data?.msg);
