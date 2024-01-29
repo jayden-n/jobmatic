af02823cbd8efae97f8e5b01791b493a28aafb9c
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-29 11:28:32 -0500

    fix(deployment): build front-end programmatically for render site

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // point to public folder for static project img
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -55,7 +55,7 @@ app.use('/api/v1/auth', authRouter);
 
 // gonna hit the front-end since you already npm run build
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+	res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 //  ================== "NOT FOUND" MIDDLEWARE  ==================
