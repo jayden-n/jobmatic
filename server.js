b2ce240c54c25675fa3ec3b0706dca697d1532db
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-29 10:28:12 -0500

    fix(deployment): create dist folder in server for render site after npm run build

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
-import express from "express";
+import express from 'express';
 const app = express();
-import morgan from "morgan";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from "cloudinary";
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from 'cloudinary';
 
 // ================== ROUTERS ==================
-import jobRouter from "./routes/jobRouter.js";
-import authRouter from "./routes/authRouter.js";
-import userRouter from "./routes/userRouter.js";
+import jobRouter from './routes/jobRouter.js';
+import authRouter from './routes/authRouter.js';
+import userRouter from './routes/userRouter.js';
 
 // ================== PUBLIC ==================
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import path from "path";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // ================== MIDDLEWARE ==================
-import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
-import { authenticateUser } from "./middleware/authMiddleware.js";
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 // ================== COULDINARY ==================
 cloudinary.config({
@@ -30,32 +30,37 @@ cloudinary.config({
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
 }
 
 // point to public folder for static project img
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
+app.get('/', (req, res) => {
+	res.send('Hello World');
 });
 
-app.get("/api/v1/test", (req, res) => {
-	res.json({ msg: "test route" });
+app.get('/api/v1/test', (req, res) => {
+	res.json({ msg: 'test route' });
 });
 
 // ================== BASE URL ==================
-app.use("/api/v1/jobs", authenticateUser, jobRouter);
-app.use("/api/v1/users", authenticateUser, userRouter);
-app.use("/api/v1/auth", authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
+app.use('/api/v1/auth', authRouter);
+
+// gonna hit the front-end since you already npm run build
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
 
 //  ================== "NOT FOUND" MIDDLEWARE  ==================
-app.use("*", (req, res) => {
-	res.status(404).json({ msg: "not found" });
+app.use('*', (req, res) => {
+	res.status(404).json({ msg: 'not found' });
 });
 
 //  ================== "ERROR" MIDDLEWARE  ==================
